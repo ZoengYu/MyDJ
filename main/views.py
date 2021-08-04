6da9b0e9d4831066d38d162d0616a244ddd16ed2
Harryu <harry.wang@cienet.com>
2021-08-04 21:55:23 +0800

    POST interact, add save & delete & Add Item function on the page

--- main/views.py
+++ main/views.py
@@ -8,6 +8,31 @@ def index(response,id):
 	ls = ToDoList.objects.get(id=id)
 	#item = ls.item_set.get(id=1)
 	#return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name,str(item.text)))
+
+	if response.method == "POST":
+		print(response.POST)
+		if response.POST.get("save"):
+			for item in ls.item_set.all():
+				if response.POST.get("c"+str(item.id)) == "on":
+					item.complete = True
+				else:
+					item.complete = False
+				
+				item.save()
+		elif response.POST.get("newItem"):
+			txt = response.POST.get("new")
+			if len(txt) > 2:
+				ls.item_set.create(text=txt, complete=False)
+			else:
+				print("invalid")
+		elif response.POST.get("delete"):
+			for item in ls.item_set.all():
+				print(item.id)
+				if response.POST.get("c"+str(item.id)) == "on":
+					delete_ID = item.id
+					print(ls.item_set.filter(id=delete_ID))
+					ls.item_set.filter(id=delete_ID).delete()
+
 	if ls.name:
 		return render(response, "main/list.html",{"ls":ls})
 	else:
