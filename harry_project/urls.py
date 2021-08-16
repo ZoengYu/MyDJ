46e3a2af514932dade17b81bafe2aea554763d3e
Harryu <harry.wang@cienet.com>
2021-08-16 17:31:07 +0800

    add register function

--- harry_project/urls.py
+++ harry_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/',register_views.register,name="register"),
     path('', include("main.urls")),
+    path('', include("django.contrib.auth.urls")),
 ]
